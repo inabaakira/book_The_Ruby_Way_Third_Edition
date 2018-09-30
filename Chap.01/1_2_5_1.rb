671d3acfe1921a3a0ec24f123f8aadba0813b9e5
INABA Akira <inaba.akira@gmail.com>
2018-09-30 12:38:04 +0000

    1.2.5 A Sample Program

---
+++ Chap.01/1_2_5_1.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+#-*- mode: ruby; coding: utf-8 -*-
+# file: 1_2_5_1.rb
+#    Created:       <2018/09/30 14:35:51>
+#    Last Modified: <2018/09/30 14:48:35>
+
+print "Please enter a temperature and scale (C of F): "
+STDOUT.flush
+str = gets
+exit if str.nil? || str.empty?
+str.chomp!
+temp, scale = str.split(" ")
+
+abort "#{temp} is not a valid number." if temp !~ /^-?\d+$/
+
+temp = temp.to_f
+case scale
+when "C", "c"
+  f = 1.8 * temp + 32
+when "F", "f"
+  c = (5.0/9.0) * (temp - 32)
+else
+  abort "Must specify C or F."
+end
+
+if f.nil?
+  puts "#{c} degrees C"
+else
+  puts "#{f} degrees F"
+end
